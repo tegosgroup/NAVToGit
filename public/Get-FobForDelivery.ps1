966d213f1e9a6e864699b64492b738c21e8cc702
Stefan Wortmann <swortmann@tegos.eu>
2020-02-12 15:50:27 +0100

    FobForDelivery working with all versions via commandline

--- public/Get-FobForDelivery.ps1
+++ public/Get-FobForDelivery.ps1
@@ -1,7 +1,7 @@
-function Import-FromGitToNAV {
+function Get-FobForDelivery {
     param (
         $useConfig,
-        [switch]$compile,
+        $Path,
         [parameter(DontShow)]
         [switch]$dark
     )
@@ -36,15 +36,11 @@ function Import-FromGitToNAV {
         }
     }
 
-    if ($compile) {
-        $config.$($config.active).CompileObjects = $true;
-    }
-
-
+    #TODO Objekte für fob wählbar
     $GUIFunctions = Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "private\gui\ImportObjectsList.ps1"
     . $GUIFunctions
 
-    Write-Host "$(Get-Date -Format "HH:mm:ss") | Started Import with configuration $($config.active)" -ForegroundColor Cyan
+    Write-Host "$(Get-Date -Format "HH:mm:ss") | Started getting fob(s) for delivery with configuration $($config.active)" -ForegroundColor Cyan
 
     $finsqlPath = Join-Path -Path $config.$($config.active).RTCpath -ChildPath "finsql.exe"
     [int]$finsqlversion = Get-ChildItem $finsqlPath | ForEach-Object { $_.VersionInfo.ProductVersion } | ForEach-Object { $_.SubString(0, $_.IndexOf(".")) }
@@ -66,36 +62,41 @@ function Import-FromGitToNAV {
             Start-Export -skipRobocopy -config $config -thirdpartyfobs $thirdpartyfobs -credential $Credential
         }
         else {
-            Start-Export -skipRobocopy -config $config -thirdpartyfobs $thirdpartyfobs
+            #Start-Export -skipRobocopy -config $config -thirdpartyfobs $thirdpartyfobs
         }
         $nav6 = $false;
     }
     Write-Host "$(Get-Date -Format "HH:mm:ss") | Export finished" -ForegroundColor Cyan
 
-    $databaseFolderPath = Join-Path -Path $config.$($config.active).TempFolder -ChildPath $config.active
-    $gitFolderPath = Get-Item $config.$($config.active).GitPath
-    $list = Compare-Folders -databaseFolder $databaseFolderPath -gitFolder $gitFolderPath -nav6 $nav6
-    $databaseName = $config.$($config.active).DatabaseName
-    $servername = $config.$($config.active).SQLServerName
+    $gitCommitId = git -C (Get-Item $config.$($config.active).GitPath) log -n1 --format="%h"
+    $decision = Write-Host "$(Get-Date -Format "HH:mm:ss") | Comparing database with git commit '$gitCommitId'."
+    $list = Compare-Folders -databaseFolder (Join-Path -Path $config.$($config.active).TempFolder -ChildPath $config.active) -gitFolder (Get-Item $config.$($config.active).GitPath) -nav6 $nav6
+    <#[System.Collections.Generic.List[String]]$list = {table\Table 0000050000.txt}
+    $list.Add("codeunit\Codeunit 0000000023.txt")
+    $list.Add("codeunit\Codeunit 0000000022.txt")
+    $list.Add("codeunit\Codeunit 0000000010.txt")
+    $list.Add("table\Table 0000000120.txt")
+    $list.Add("table\Table 0000000130.txt")
+    $list.Add("table\Table 0000000121.txt") #>
 
     if ([long]$list.Count -gt 0) {
         while ($true) {
-            $decision = Read-Host "$(Get-Date -Format "HH:mm:ss") | Found $($list.Count) changed objects. Enter (a) to apply all changed objects, (s) to select the objects you want to import or (c) to cancel this operation"
+            $decision = Read-Host "$(Get-Date -Format "HH:mm:ss") | Found $($list.Count) changed objects in database compared to git commit '$gitCommitId'. Enter (a) to write all changed objects to fob, (s) to select the objects you want to write or (c) to cancel this operation"
             if ($decision -eq "a") {
-                [System.Collections.Generic.List[String]]$selectedObjectsList = Set-Nav-Changes -databaseName $databaseName -servername $servername -finsqlPath $finsqlPath -list $list -nav6 $nav6 -credential $Credential -config $config
+                Get-FobAndDeleteTxt -config $config -FobFolderPath $Path -list $list -nav6 $nav6 -credential $Credential
                 break
             }
             elseif ($decision -eq "s") {
                 Write-Host "$(Get-Date -Format "HH:mm:ss") | Collecting changed object information"
-                Open-ObjectList -git $gitFolderPath -temp $databaseFolderPath -CompareToolPath ($guiconfig.CompareToolPath) -CompareToolParam ($guiconfig.CompareToolParameter) -dark:$dark
-                Initialize-GridViewWithPsObjects -dataList (Show-Changed-Objects -databasePath $databaseFolderPath -gitPath $gitFolderPath -GitToDatabase) -dark:$dark
+                Open-ObjectList -git (Get-Item $config.$($config.active).GitPath) -temp (Join-Path -Path $config.$($config.active).TempFolder -ChildPath $config.active) -CompareToolPath ($guiconfig.CompareToolPath) -CompareToolParam ($guiconfig.CompareToolParameter) -dark:$dark
+                Initialize-GridViewWithPsObjects -dataList (Show-Changed-Objects -databasePath (Join-Path -Path $config.$($config.active).TempFolder -ChildPath $config.active) -gitPath (Get-Item $config.$($config.active).GitPath)) -dark:$dark
                 [System.Collections.Generic.List[String]]$selectedObjectsList = Show-Dialog
                 if (0 -eq $selectedObjectsList.Count) {
-                    Write-Host "$(Get-Date -Format "HH:mm:ss") | No objects have been selected for Import"
+                    Write-Host "$(Get-Date -Format "HH:mm:ss") | No objects have been selected for fob creation"
                     break
                 }
                 else {
-                    [System.Collections.Generic.List[String]]$selectedObjectsList = Set-Nav-Changes -databaseName $databaseName -servername $servername -finsqlPath $finsqlPath -list $selectedObjectsList  -nav6 $nav6 -credential $Credential -config $config
+                    Get-FobAndDeleteTxt -config $config -FobFolderPath $Path -list $selectedObjectsList -nav6 $nav6 -credential $Credential
                     break
                 }
             }
@@ -104,34 +105,12 @@ function Import-FromGitToNAV {
                 break
             }
         }
-        if ($config.$($config.active).CompileObjects -and -not $nav6){
-            Set-ObjectsCompiled -databaseName $databaseName -servername $servername -finsqlPath $finsqlPath -selectedObjectsList $selectedObjectsList  -nav6 $nav6 -credential $Credential -config $config
-        } elseif (($config.$($config.active).CompileObjects -and $nav6)) {
-            Write-Host "$(Get-Date -Format "HH:mm:ss") | Start compiling $($selectedObjectsList.Count) objects" -ForegroundColor Cyan
-            $failedObjectsList = Set-Nav6ObjectsCompiled -DatabaseName $databaseName -SelectedObjectsList $selectedObjectsList            
-            if ($failedObjectsList.length -gt 0) {
-                $regex = [Regex]::new("([^\\]*).*\s([0-9]*).txt")
-                $failedObjectsList | ForEach-Object {
-                    $match = $regex.Matches($_)[0]
-                    $Type = $match.Groups[1].Value
-                    [long]$Id = $match.Groups[2].Value
-                    Write-Host "$(Get-Date -Format "HH:mm:ss") | Error while trying to compile $Type $Id" -ForegroundColor Red
-                    $selectedObjectsList.Remove($_) > $null
-                }
-                $selectedObjectsList | ForEach-Object {
-                    $match = $regex.Matches($_)[0]
-                    $Type = $match.Groups[1].Value
-                    [long]$Id = $match.Groups[2].Value
-                    Write-Host "$(Get-Date -Format "HH:mm:ss") | Successfully compiled $Type $Id." -ForegroundColor Green
-                }
-            }
-        }
     }
     else {
-        Write-Host "$(Get-Date -Format "HH:mm:ss") | No import or delete in database needed - git and database are in sync"
+        Write-Host "$(Get-Date -Format "HH:mm:ss") | No changed objects to export - git and database are the same"
     }
     
     Write-Host("$(Get-Date -Format "HH:mm:ss") | Removing tempfolder " + $config.$($config.active).TempFolder) -ForegroundColor Cyan
     Remove-Item -Recurse -Force -Path (Join-Path -Path $config.$($config.active).TempFolder -ChildPath $config.active)
-    Write-Host("$(Get-Date -Format "HH:mm:ss") | Import finished. You can close this window.") -ForegroundColor Green
+    Write-Host("$(Get-Date -Format "HH:mm:ss") | Writing fob(s) for delivery completed. You can close this window.") -ForegroundColor Green
 }
