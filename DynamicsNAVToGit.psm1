b09f54756d96b6275063bfaed234fd73e8a7b5a7
nbuko <nbukowski@tegos.eu>
2020-02-14 11:20:12 +0100

    Fixed Input Error

--- DynamicsNAVToGit.psm1
+++ DynamicsNAVToGit.psm1
@@ -12,12 +12,12 @@ $autoUpdater = Join-Path -Path $PSScriptRoot -ChildPath "private\AutoUpdater.ps1
 . $autoUpdater
 
 if (Get-UpdateAvailable) {
-    while (-not $decision -eq "y" -and -not $decision -eq "n") {
+    while ((-not ($decision -eq "y")) -and (-not ($decision -eq "n"))) {
         $decision = Read-Host "$(Get-Date -Format "HH:mm:ss") | Found new version. Do you want to apply the update? [y/n]"
         if ($decision -eq "y") {
             Invoke-UpdateProcess
-        } elseif (-not $decision -eq "y" -and -not $decision -eq "n") {
-            Write-Host("$(Get-Date -Format "HH:mm:ss") | Not expected input") -ForegroundColor Red
+        } elseif ((-not ($decision -eq "y")) -and (-not ($decision -eq "n"))) {
+            Write-Host("$(Get-Date -Format "HH:mm:ss") | Wrong input") -ForegroundColor Red
         }
     }
 }
