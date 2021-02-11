7094805b191c4bfee94128a4036a279806d9015a
Stefan Wortmann <swortmann@tegos.eu>
2021-02-11 17:35:32 +0100

    Fixed file-splitting for edge cases occuring in OPP objects

--- lib/src/SplitNavObjects/SplitNavObjects/SplitNavObjects.cs
+++ lib/src/SplitNavObjects/SplitNavObjects/SplitNavObjects.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.IO;
 using System.Management.Automation;
+using System.Text.RegularExpressions;
 
 namespace SplitNavObjects
 {
@@ -55,26 +56,29 @@ namespace SplitNavObjects
                 file = System.IO.Path.Combine(file, "Export.txt");
                 if (File.Exists(file))
                 {
-                    string[] separator = { $"OBJECT {actualObjName} " };
+                    string pattern = @"^OBJECT " + actualObjName + " ";
 
                     Encoding enc = Encoding.GetEncoding(850);
-                    string readStr = File.ReadAllText(file, enc);
-                    string[] objects = readStr.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                    string readStr = File.ReadAllText(file, enc);                    
+                    string[] objects = Regex.Split(readStr, pattern, RegexOptions.Multiline | RegexOptions.Compiled);
                     long id;
                     string filepath;
                     byte[] content;
                     FileStream stream;
                     foreach (string o in objects)
                     {
-                        content = enc.GetBytes($"OBJECT {actualObjName} " + o);
-                        long.TryParse(o.Substring(0, o.IndexOf(" ")), out id);
-                        filepath = System.IO.Path.Combine(Path, $"{Type}/{Type} {id.ToString("0000000000")}.txt");
-                        LogMessage($"Filepath: {filepath}");
-                        stream = new FileStream(filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
-                        stream.SetLength(0); //Delete all data
-                        stream.Write(content, 0, content.Length); //Write Data
-                        stream.Flush();
-                        stream.Close();
+                        if (o != "" && o != pattern)
+                        {
+                            content = enc.GetBytes($"OBJECT {actualObjName} " + o);
+                            long.TryParse(o.Substring(0, o.IndexOf(" ")), out id);
+                            filepath = System.IO.Path.Combine(Path, $"{Type}/{Type} {id.ToString("0000000000")}.txt");
+                            LogMessage($"Filepath: {filepath}");
+                            stream = new FileStream(filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                            stream.SetLength(0); //Delete all data
+                            stream.Write(content, 0, content.Length); //Write Data
+                            stream.Flush();
+                            stream.Close();
+                        }
                     }
                     File.Delete(file);
                 }
