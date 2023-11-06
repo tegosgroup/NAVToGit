4554e095b419dfed1c8b670f359b90b1a81e4feb
Stefan Wortmann <60692534+swortmann@users.noreply.github.com>
2023-11-06 16:33:49 +0100

    Revert "Filter Length Fix"

--- private/Functions.ps1
+++ private/Functions.ps1
@@ -188,64 +188,15 @@ function Start-Export {
         foreach ($key in $objectWithFilters.Keys) {
             $value = $objectWithFilters[$key]
             Write-Host "$(Get-Date -Format "HH:mm:ss") | Started exporting ${key}s with filter '$value'"
-
-            $Array = $filter.Split("|")
-
-            $filters = @()
-            $counter = 0
-            $SB = New-Object -TypeName System.Text.StringBuilder
-
-            foreach ($item in $Array) {
-                if (($counter -lt 10) -and !($item -eq $Array[$Array.length - 1])) {
-                    [void]$SB.Append($item + "|")
-                    $counter++
-                }
-                else {
-                    [void]$SB.Append($item)
-                    $filters += $SB.ToString()
-                    $counter = 0
-                    $SB.Clear() > $null
-                }
-            }
-
-            $number = 0
-            foreach ($filterString in $filters) {
-                Start-Job $Export -Name $type -ArgumentList $type, $filterString, $TempRepo, $finsqlPath, $sqlServername, $databaseName, $credential, $number > $null
-                $number++
-                Start-Sleep -Milliseconds 200
-            }
+            $filter = "Type=$key;$value"
+            Start-Job $Export -Name $type -ArgumentList $key, $filter, $TempRepo, $finsqlPath, $sqlServername, $databaseName, $credential > $null
         }
     }
     else {
         $filter = Get-NoThirdPartyFilter -thirdpartyfobs $thirdpartyfobs
         foreach ($type in $objectTypes) {
             Write-Host "$(Get-Date -Format "HH:mm:ss") | Started exporting $($type)s"
-
-            $Array = $filter.Split("|")
-
-            $filters = @()
-            $counter = 0
-            $SB = New-Object -TypeName System.Text.StringBuilder
-
-            foreach ($item in $Array) {
-                if (($counter -lt 10) -and !($item -eq $Array[$Array.length - 1])) {
-                    [void]$SB.Append($item + "|")
-                    $counter++
-                }
-                else {
-                    [void]$SB.Append($item)
-                    $filters += $SB.ToString()
-                    $counter = 0
-                    $SB.Clear() > $null
-                }
-            }
-
-            $number = 0
-            foreach ($filterString in $filters) {
-                Start-Job $Export -Name $type -ArgumentList $type, $filterString, $TempRepo, $finsqlPath, $sqlServername, $databaseName, $credential, $number > $null
-                $number++
-                Start-Sleep -Milliseconds 200
-            }
+            Start-Job $Export -Name $type -ArgumentList $type, "Type=$type;ID=$filter", $TempRepo, $finsqlPath, $sqlServername, $databaseName, $credential > $null
         }
         if ($config.$($config.active).EnableThirdPartyFobExport) {
             Write-Host "$(Get-Date -Format "HH:mm:ss") | Started exporting Third-Party fobs"
@@ -262,14 +213,6 @@ function Start-Export {
 
     Remove-Job *
 
-    foreach ($type in $objectTypes) {
-        if (Test-Path -Path $($TempRepo + "\" + $type.ToLower() + "\*")) {
-            $ExportCache = Get-Content -Path $($TempRepo + "\" + $type.ToLower() + "\*") -Filter *.txt
-            Remove-Item -Path $($TempRepo + "\" + $type.ToLower() + "\*") -Filter *.txt
-            Set-Content -Path $($TempRepo + "\" + $type.ToLower() + "\Export.txt") $ExportCache
-        }
-    }
-    
     [String]$logFile = Get-Content (Join-Path -Path $TempRepo -ChildPath "navcommandresult.txt")
     if (-not $logFile.contains("successfully")) {
         Write-Host "$(Get-Date -Format "HH:mm:ss") | Error while trying to Export:`n"(Get-Content $logFile.Substring($logFile.LastIndexOf(":") - 1)) -ForegroundColor Red
